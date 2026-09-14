6186d4c7711bdddd55905b64a1fd7b080785f410
bhavagnareddy369 <bhavagnyareddy369@gmail.com>
2026-09-14 19:43:36 +0530

    Add files via upload

---
+++ 2520030062_Practical/Practical_1/week3/week3.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int main()
+{
+    pid_t pid;
+
+    printf("Parent process started\n");
+    printf("Parent PID: %d\n", getpid());
+    printf("Parent PPID: %d\n", getppid());
+
+    pid = fork();
+
+    if (pid < 0)
+    {
+        perror("fork failed");
+        return 1;
+    }
+
+    if (pid == 0)
+    {
+        // Child process
+        printf("\n--- Child Process ---\n");
+        printf("Child PID: %d\n", getpid());
+        printf("Child PPID: %d\n", getppid());
+
+        printf("Child is running...\n");
+        sleep(5);
+
+        printf("Child finished execution.\n");
+        exit(0);
+    }
+    else
+    {
+        // Parent process
+        printf("\n--- Parent Process ---\n");
+        printf("Parent PID: %d\n", getpid());
+        printf("Child PID: %d\n", pid);
+
+        printf("Parent is waiting for child...\n");
+
+        wait(NULL);
+
+        printf("Child has terminated.\n");
+        printf("Parent process completed.\n");
+    }
+
+    return 0;
+}
